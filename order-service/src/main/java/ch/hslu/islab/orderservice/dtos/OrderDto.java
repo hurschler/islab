42ed3eda14292e7505d89a032bf12994661560ee
PatrickRichner <patrick.richner@stud.hslu.ch>
2020-09-25 23:30:08 +0200

    Add OrderDto

---
+++ order-service/src/main/java/ch/hslu/islab/orderservice/dtos/OrderDto.java
@@ -0,0 +1,23 @@
+package ch.hslu.islab.orderservice.dtos;
+
+public class OrderDto {
+
+    private Integer orderId;
+    private Integer productId;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+}
