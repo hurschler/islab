84bda174755360c4286273040ef57cac5bc2d0f7
PatrickRichner <patrick.richner@stud.hslu.ch>
2020-09-28 21:07:20 +0200

    Add modelmapper to orderservice

--- order-service/src/main/java/ch/hslu/islab/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/ch/hslu/islab/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package ch.hslu.islab.orderservice;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrderServiceApplication {
@@ -15,5 +17,9 @@ public class OrderServiceApplication {
 		logger.info("OrderServiceApplication started successfully.");
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 }
