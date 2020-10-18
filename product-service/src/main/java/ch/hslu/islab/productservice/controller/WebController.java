316525c84a9c3596f12e908b31bcd933ea7e7e28
PatrickRichner <patrick.richner@stud.hslu.ch>
2020-10-18 15:47:59 +0200

    Add Product WebController

--- product-service/src/main/java/ch/hslu/islab/productservice/controller/WebController.java
+++ product-service/src/main/java/ch/hslu/islab/productservice/controller/WebController.java
@@ -1,6 +1,11 @@
 package ch.hslu.islab.productservice.controller;
 
 import java.security.Principal;
+import ch.hslu.islab.productservice.dao.ProductDao;
+import ch.hslu.islab.productservice.entities.Product;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,44 +16,105 @@ import ch.hslu.islab.productservice.entities.Customer;
 @Controller
 public class WebController {
 
-    @Autowired
-    private CustomerDAO customerDAO;
+  private static final Logger logger = LogManager.getLogger(WebController.class);
 
-    @GetMapping(path = "/")
-    public String index() {
-        return "external";
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @Autowired
+  private CustomerDAO customerDAO;
+
+  @Autowired
+  private ProductDao productDao;
+
+  @GetMapping(path = "/")
+  public String index() {
+    return "external";
+  }
+/*
+  @GetMapping(path = "/products")
+  public String customers(Principal principal, Model model) {
+    addCustomers();
+    Iterable<Customer> customers = customerDAO.findAll();
+    model.addAttribute("customers", customers);
+    if (principal != null) {
+      model.addAttribute("username", principal.getName());
     }
+    return "customers";
+  }
 
-    @GetMapping(path = "/customers")
-    public String customers(Principal principal, Model model) {
-        addCustomers();
-        Iterable<Customer> customers = customerDAO.findAll();
-        model.addAttribute("customers", customers);
-        if (principal != null) {
-            model.addAttribute("username", principal.getName());
-        }
-        return "customers";
+ */
+
+  @GetMapping(path = "/products")
+  public String getProducts(Principal principal, Model model) {
+    addProducts();
+    Iterable<Product> products = productDao.findAll();
+    model.addAttribute("products", products);
+    if (principal != null) {
+      model.addAttribute("username", principal.getName());
     }
+    logger.info("Gett all products" + products);
+    return "products";
+  }
 
-    // add customers for demonstration
-    public void addCustomers() {
 
-        Customer customer1 = new Customer();
-        customer1.setAddress("1111 foo blvd");
-        customer1.setName("Foo Industries");
-        customer1.setServiceRendered("Important services");
-        customerDAO.save(customer1);
-
-        Customer customer2 = new Customer();
-        customer2.setAddress("2222 bar street");
-        customer2.setName("Bar LLP");
-        customer2.setServiceRendered("Important services");
-        customerDAO.save(customer2);
-
-        Customer customer3 = new Customer();
-        customer3.setAddress("33 main street");
-        customer3.setName("Big LLC");
-        customer3.setServiceRendered("Important services");
-        customerDAO.save(customer3);
+/*
+  @GetMapping(path = "/customers")
+  public String getProductDtos(Principal principal, Model model) {
+    addProducDto();
+    Iterable<Product> products = productDao.findAll();
+    List<ProductDto> productDtos = new ArrayList<>();
+    for (Product productInList : products) {
+      ProductDto productDto = modelMapper.map(productInList, ProductDto.class);
+      productDtos.add(productDto);
+      logger.info("Get all product of the product DB.");
     }
+    model.addAttribute("productDtos", productDtos);
+    if (principal != null) {
+      model.addAttribute("username", principal.getName());
+    }
+    return "productDtos";
+  }
+
+ */
+
+  public void addProducts(){
+    Product product1 = new Product();
+    product1.setDescription("Zahnpasta");
+    product1.setPrice(10);
+    productDao.save(product1);
+
+    Product product2 = new Product();
+    product2.setDescription("Barilla Pasta");
+    product2.setPrice(35);
+    productDao.save(product2);
+
+    Product product3 = new Product();
+    product3.setDescription("Rasenmäher");
+    product3.setPrice(150);
+    productDao.save(product3);
+  }
+
+
+  // add customers for demonstration
+  public void addCustomers() {
+
+    Customer customer1 = new Customer();
+    customer1.setAddress("1111 foo blvd");
+    customer1.setName("Foo Industries");
+    customer1.setServiceRendered("Important services");
+    customerDAO.save(customer1);
+
+    Customer customer2 = new Customer();
+    customer2.setAddress("2222 bar street");
+    customer2.setName("Bar LLP");
+    customer2.setServiceRendered("Important services");
+    customerDAO.save(customer2);
+
+    Customer customer3 = new Customer();
+    customer3.setAddress("33 main street");
+    customer3.setName("Big LLC");
+    customer3.setServiceRendered("Important services");
+    customerDAO.save(customer3);
+  }
 }
